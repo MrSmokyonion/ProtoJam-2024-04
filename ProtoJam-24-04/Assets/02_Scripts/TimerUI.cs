788f75e38a72e1224629c38bb2b229446de32a56
Blue3Ray <xoals1126@gmail.com>
2024-04-09 17:17:35 +0900

    Timer UI기능 추가 초 입력 또는 분초 입력을 받으면 화면에 표시하는 기능 추가

---
+++ ProtoJam-24-04/Assets/02_Scripts/TimerUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        FindAnyObjectByType<Test_01_TimerUI>().onTime += RefreshTimer;          // 나중에 타이머 담당할 스크립트/오브젝트과 연결
+    }
+
+    /// <summary>
+    /// 타이머 변할때 호출되는 함수(자동으로 분, 초 계산해줌)
+    /// </summary>
+    /// <param name="time">시간</param>
+    void RefreshTimer(int time)
+    {
+        int mint = time / 60;
+        int sec = time % 60;
+
+        text.text = $"{mint:D2}:{sec:D2}";
+    }
+
+    /// <summary>
+    /// 타이머 변할때 호출되는 함수(분, 초 입력)
+    /// </summary>
+    /// <param name="mint">분</param>
+    /// <param name="sec">초</param>
+    void RefreshTimer(int mint, int sec)
+    {
+        text.text = $"{mint:D2}:{sec:D2}";
+    }
+}
