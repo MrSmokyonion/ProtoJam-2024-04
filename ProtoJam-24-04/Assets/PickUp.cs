e7b02b271d5528c942e70e237ae944c53fdcef48
minsungsmooth <minsongshin@naver.com>
2024-04-11 22:22:40 +0900

    들고 있는 아이템 놓는 기능
    던지는 기능으로 바꿀예정

--- ProtoJam-24-04/Assets/PickUp.cs
+++ ProtoJam-24-04/Assets/PickUp.cs
@@ -5,28 +5,38 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     public Transform holdPos;
-    public GameObject holdTrigger;
+    public GameObject Player;
 
     private GameObject heldObj;
     private Rigidbody heldObjRb;
+    private Vector3 Trigger = new Vector3(0.9f, 0.5f, 1f);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        heldObj = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) {
-            CheckTrigger();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if (heldObj == null)
+            {
+                CheckTrigger();
+            }
+            else
+            {
+                DropObject();
+            }
         }
     }
 
-    private void CheckTrigger()
+    private void CheckTrigger() //들 수 있는 아이템 탐색
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 10f);
+        Collider[] colliders = Physics.OverlapBox(transform.position, Trigger);
 
         foreach (Collider collider in colliders)
         {
@@ -34,23 +44,29 @@ public class PickUp : MonoBehaviour
             if (collider.gameObject.CompareTag("canPickUp"))
             {
                 Debug.Log("Check");
-                if (heldObj == null)
-                {
-                    heldObj = collider.gameObject;
-                    Pickup();
-                }
+                heldObj = collider.gameObject;
+                Pickup();
+
+                break;
             }
 
         }
     }
 
-    private void Pickup()
+    private void Pickup() //들기
     {
         heldObjRb = heldObj.GetComponent<Rigidbody>();
         heldObjRb.isKinematic = true;
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), Player.GetComponent<Collider>(), true);
         heldObj.transform.position = holdPos.position;
         heldObjRb.transform.parent = holdPos.transform;
     }
 
-    
+    private void DropObject() //내려놓기
+    {
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), Player.GetComponent<Collider>(), false);
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
 }
