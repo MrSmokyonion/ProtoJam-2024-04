f55709c2e592fc79e567face9fe68c00e58dbbe7
DESKTOP-GVL0O4M\hwaya <hwayanggames@gmail.com>
2024-04-17 22:36:59 +0900

    스파인 애니메이션 적용
    애니메이터에서 수정 가능

---
+++ ProtoJam-24-04/Assets/02_Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,60 @@
+using Spine;
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private Animator animator;
+
+    string state;
+    public Rigidbody getRigid()
+    {
+        return playerRb;
+    }
+
+    private void Awake()
+    {
+        playerRb = this.GetComponent<Rigidbody>();
+        animator = this.transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        ChangeState(state);
+    }
+    public void ReceisveState(string target)
+    {
+        state = target;
+    }
+
+    private void ChangeState(string target)
+    {
+        switch(target)
+        {
+            case "Idle":
+                animator.SetBool("isIdle", true);
+                animator.SetBool("isMove", false);
+                animator.SetBool("isCarring", false);
+
+                Debug.Log("Animation" + target);
+                break;
+            case "Move":
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isMove", true);
+                animator.SetBool("isCarring", false);
+                Debug.Log("Animation" + target);
+                break;
+            case "Carring":
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isMove", false);
+                animator.SetBool("isCarring", true);
+                Debug.Log("Animation" + target);
+                break;
+        }
+    }
+
+}
