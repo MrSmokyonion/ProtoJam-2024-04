25d20eaccb817738bf3aab64c516cbec55f33b70
Blue3Ray <xoals1126@gmail.com>
2024-04-20 21:16:17 +0900

    Result 씬 버튼 기능 통합 완료 퍼블리시에 있는게 아닌 제 테스트 씬에 있는게 최신 입니다.
    크래딧 이름 디코 프로플 기준으로 1차적으로 적용했습니다.

--- ProtoJam-24-04/Assets/02_Scripts/UI/GameOverMenuUI.cs
+++ ProtoJam-24-04/Assets/02_Scripts/UI/GameOverMenuUI.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class GameOverMenuUI : MenuUI
 {
+    ResultChairLoader resultChairLoader;
+
+    private void Start()
+    {
+        resultChairLoader = FindAnyObjectByType<ResultChairLoader>();
+    }
+
     protected override void EnterButton()
     {
         switch(cursorIndex)
         {
             case 0:
+                // 오브젝트 타겟 오른쪽으로 이동
+                resultChairLoader.MoveLeft();
+                break;
+            case 1:
                 // 게임을 다시하기
                 SceneController.Ins.GoToScene(SceneType.Game);
                 break ;
-            case 1:
+            case 2:
+                // 메인으로 돌아가기
                 SceneController.Ins.GoToSceneWithoutLoad(SceneType.Main);
+                break;
+            case 3:
+                // 오브젝트 타겟 왼쪽으로 이동
+                resultChairLoader.MoveRight();
                 break;
 
         }
